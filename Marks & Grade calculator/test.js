7f7ea90af3c7fa1a21346fbdad31440645f8b02d
Mohsin Javaid <muhsanjavaid@gmail.com>
2022-02-24 12:02:57 +0500

    Marks calculator

---
+++ Marks & Grade calculator/test.js
@@ -0,0 +1,21 @@
+function othername() {
+    var input = document.getElementById("userInput").value;
+    if (input < 50) {
+        document.getElementById("output").innerHTML = "Your marks " + input + " Grade is F";
+    }
+    else if (input >= 50 && input < 60) {
+        document.getElementById("output").innerHTML = "Your marks " + input + " Grade is D";
+    }
+    else if (input >= 60 && input < 70) {
+        document.getElementById("output").innerHTML = "Your marks " + input + " Grade is C";
+    }
+    else if (input >= 70 && input < 80) {
+        document.getElementById("output").innerHTML = "Your marks " + input + " Grade is B";
+    }
+    else if (input >= 80 && input < 90) {
+        document.getElementById("output").innerHTML = "Your marks " + input + " Grade is A";
+    }
+    else if (input > 90) {
+        document.getElementById("output").innerHTML = "Your marks " + input + " Grade is A+";
+    }
+}
