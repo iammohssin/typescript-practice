242808233ba7a2340a2a12dfa11bb494d2e557b8
Mohsin Javaid <muhsanjavaid@gmail.com>
2022-02-25 23:29:36 +0500

    Reverse an array challange

---
+++ Reverse an array/test.js
@@ -0,0 +1,4 @@
+var arr = ["aslam", "akram", "faheem", "nouman"];
+for (var i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+}
---
+++ Reverse an array/test.ts
@@ -0,0 +1,8 @@
+//Reverse an array
+ 
+let arr:string[] = ["aslam", "akram", "faheem", "nouman"];
+
+for (let i = arr.length - 1; i >= 0; i--)
+ {
+	console.log(arr[i]);
+}
